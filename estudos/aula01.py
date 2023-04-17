8d35729cc3b3bcfba09a48cc7c98b6d6297f6bed
Ives Gabriel Figueiredo Santos Pinto <122233979+IvesFigueiredo@users.noreply.github.com>
2023-04-17 17:08:37 -0300

    Create aula01.py

---
+++ estudos/aula01.py
@@ -0,0 +1,8 @@
+n = input("Digite algo vai checar se é número ou não: ")
+print(n.isnumeric())
+
+m = input("Digite outra coisa, vai checar se é alfabético ou não: ")
+print(m.isalpha())
+
+b = input("Digite algo nnovamente, vai checar se é alfanumérico ou não: ")
+print(b.isalnum())
