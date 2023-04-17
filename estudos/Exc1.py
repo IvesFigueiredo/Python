6838675d99d26e488ac52f23cd078a8dcf714c88
Ives Gabriel Figueiredo Santos Pinto <122233979+IvesFigueiredo@users.noreply.github.com>
2023-04-17 16:05:16 -0300

    Update Exc1.py

--- estudos/Exc1.py
+++ estudos/Exc1.py
@@ -2,4 +2,6 @@ nome = input("Qual é o seu nome?\n")
 idade = input("Qual é a sua idade?\n")
 peso = input("Quantos kilos você pesa?\n")
 
-print("\nMeu nome: ", nome, "\nMinha idade: ", idade, "\nMeu peso: ", peso)
+print("\nMeu nome: ", nome)
+print("Minha idade: ", idade, "anos.")
+print("Meu peso: ", peso, "kilos.")
