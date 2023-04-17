28a8dcf5420db0a142c080c2398ab3f434cb9769
Ives Gabriel Figueiredo Santos Pinto <122233979+IvesFigueiredo@users.noreply.github.com>
2023-04-17 17:08:40 -0300

    Update des3.py

--- estudos/des3.py
+++ estudos/des3.py
@@ -2,4 +2,8 @@ num1 = int(input("Digite um número para realizar a soma: "))
 num2 = int(input("Digite o segundo número: "))
 
 num3 = num1+num2
-print("Soma:", num3)
+print("Soma entre {} e {} é {}".format(num1, num2, num3))
+
+print("Teste para booleano, se há ou não um número na variável.\n")
+n = bool(input('\n\nDigite um valor: '))
+print(n)
