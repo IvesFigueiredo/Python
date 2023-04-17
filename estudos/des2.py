9a4224dd66b09718067e215e599cb2bd32e192f7
Ives Gabriel Figueiredo Santos Pinto <122233979+IvesFigueiredo@users.noreply.github.com>
2023-04-17 16:05:13 -0300

    Create des2.py

---
+++ estudos/des2.py
@@ -0,0 +1,5 @@
+dia = input("Informe o dia do seu nascimento: ")
+mes = input("Informe o mes : ")
+ano = input("Informe também o ano: ")
+
+print("\nVocê nasceu no dia", dia, "/", mes, "/", ano, "correto?")
