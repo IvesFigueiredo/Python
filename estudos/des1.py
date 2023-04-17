64cb5a3e33c7611bafb8e73adf9f7d4aac82a481
Ives Gabriel Figueiredo Santos Pinto <122233979+IvesFigueiredo@users.noreply.github.com>
2023-04-17 16:26:20 -0300

    Update des1.py

--- estudos/des1.py
+++ estudos/des1.py
@@ -1,2 +1,4 @@
 nome = input("Informe o seu nome: ")
-print("Olá", nome, ". Seja bem vindo a linguagem Python.")
+print("Olá", nome, "! Seja bem vindo a linguagem Python.")
+
+print('\nOlá {}! Seja bem vindo a Linguagem Python.'.format(nome))
